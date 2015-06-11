314bb7b2f08f9e52075e0637b6b71cc8728dde9b
Neal Morton <neal.morton@austin.utexas.edu>
2015-06-11 12:28:02 -0500

    subjutil.py: removed old standalone functions; removed global attributes from SubjLog; added write method

--- utils/subjutil.py
+++ utils/subjutil.py
@@ -5,55 +5,6 @@ import subprocess as sub
 from datetime import datetime
 from glob import glob
 
-def run_logged_cmd(cmd, cmdfile, dry_run=False):
-
-    outfile = open(cmdfile, 'a')
-    subcode = cmdfile.split('/')[-2]
-    outfile.write('\n%s: Running: ' % subcode + cmd + '\n')
-    if not dry_run:
-        p = sub.Popen(cmd.split(' '), stdout=sub.PIPE, stderr=sub.PIPE)
-        output, errors = p.communicate()
-        outfile.write('%s: Output: ' % subcode + output + '\n')
-        if errors:
-            outfile.write('%s: ERROR: ' % subcode + errors + '\n')
-            print '%s: ERROR: ' % subcode + errors
-    outfile.close()
-
-def get_log_file(subject, base, rm_existing=False):
-    study_dir = os.environ['STUDYDIR']
-    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    filename = base + '_' + timestamp + '.log'
-    log_dir = os.path.join(study_dir, subject, 'logs')
-    log_file = os.path.join(log_dir, filename)
-
-    if rm_existing:
-        existing = glob(os.path.join(log_dir, '%s_*.log' % base))
-        for filepath in existing:
-            os.remove(filepath)
-    
-    return log_file
-
-def log_message(message, log_file):
-    outfile = open(log_file, 'a')
-    outfile.write(message + '\n')
-    outfile.close()
-
-def get_std_dirs(subject):
-
-    study_dir = os.environ['STUDYDIR']
-    subj_dir = os.path.join(study_dir, subject)
-    
-    dirnames = ['anatomy', 'behav', 'BOLD', 'DTI', 'fieldmap', 'logs',
-                'model', 'raw']
-    d = dict()
-    for dirname in dirnames:
-        d[dirname.lower()] = os.path.join(subj_dir, dirname)
-
-    d['anatomy.antsreg.xfm'] = os.path.join(d['anatomy'], 'antsreg',
-                                            'transforms')
-    d['anatomy.antsreg.data'] = os.path.join(d['anatomy'], 'antsreg', 'data')
-    return d
-
 class SubjPath:
     """Information about subject directory structure."""
 
@@ -84,10 +35,6 @@ class SubjPath:
 
 class SubjLog:
     """Class for logging subject processing."""
-
-    log_file = ''
-    dry_run = False
-    subject = ''
 
     def __init__(self, subject, base, rm_existing=False, dry_run=False):
 
@@ -121,4 +68,9 @@ class SubjLog:
             if errors:
                 outfile.write('ERROR: ' + errors + '\n')
                 print '%s: ERROR: ' % self.subject + errors
+        outfile.close()
+
+    def write(self, message):
+        outfile = open(self.log_file, 'a')
+        outfile.write(message + '\n')
         outfile.close()
