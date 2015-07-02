365fdeec2c552b176ae97b9aa8b03838e2a00aa7
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-02 12:29:40 -0500

    run_subjs.sh: rewrote implementation; added documenation

--- utils/run_subjs.sh
+++ utils/run_subjs.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-for subj in $SUBJIDS; do
-    command="$1 $subj"
-    eval $command
+if [ $# -lt 1 ]; then
+    echo "run_subjs.sh   Run a command on multiple subjects."
+    echo
+    echo "Usage: run_subjs.sh commands [subjects]"
+    echo
+    echo "In the commands string, any '{}' will be replaced with"
+    echo "subject identifier. Takes subject numbers (e.g. 1, 2)"
+    echo "and constructs them in the format [study]_DD, e.g. bender_01."
+    echo "If the environment variable SUBJNOS is set, that will"
+    echo "be used to set the subjects list."
+    exit 1
+fi
+
+command="$1"
+
+if [ $# -lt 2 ]; then
+    nos="$SUBJNOS"
+else
+    nos="$2"
+fi
+
+if [ -z "$nos" ]; then
+    echo "Error: must indicate subject numbers to include."
+    exit 1
+fi
+
+for no in $nos; do
+    subject=${STUDY}_`printf "%02d" $no`
+    subj_command=`echo $command | sed s/{}/$subject/g`
+    eval $subj_command
 done
 
