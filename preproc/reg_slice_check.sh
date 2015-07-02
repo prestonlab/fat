a7ff4ca4ec263256faad39f50e8e721168ea9f08
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-02 12:33:23 -0500

    reg_slice_check.sh: style fixes; added docstring

--- preproc/reg_slice_check.sh
+++ preproc/reg_slice_check.sh
@@ -8,15 +8,18 @@
 # usage: reg_slice_check.sh image1 image2 pngdir pngout
 # e.g.:  reg_slice_check.sh anatomy/antsreg/data/funcspace/orig.nii.gz BOLD/pre_1/bold_mcf_brain_vol1.nii.gz anatomy/antsreg/checks orig2func1.png
 
+if [ $# -lt 4 ]; then
+    echo "Usage: reg_slice_check.sh image1 image2 pngdir pngout"
+    exit 1
+fi
+
 DIST=".35 .45 .55 .65"
 AXIS="x y z"
 
 N=0
 FILES=""
-for a in $AXIS
-do
-    for d in $DIST
-    do
+for a in $AXIS; do
+    for d in $DIST; do
 	N=$((N+1))
 	slicer $1 $2 -s 2 -$a $d $3/tempslice$N.png
 	FILES="$FILES $3/tempslice$N.png"
@@ -24,8 +27,7 @@ do
 done
 
 C=""
-for f in $FILES
-do
+for f in $FILES; do
     C="$C + $f"
 done
 
