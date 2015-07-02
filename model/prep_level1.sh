34f31e1ea507529882a3f8512b7c0c2cc6869da6
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-02 12:35:29 -0500

    prep_level1.sh: changed to use standard env vars

--- model/prep_level1.sh
+++ model/prep_level1.sh
@@ -5,12 +5,11 @@ then
     echo "Prepare level 1 analysis from an FSL template"
     echo
     echo "Usage:"
-    echo "`basename $0` [template] [studydir] [model] [subjids] [runids]"
+    echo "`basename $0` [template] [model] [runids]"
     echo
     echo "Inputs:"
-    echo "studydir   path to base study directory"
+    echo "template   path to FSF template"
     echo "model      name of statistical model"
-    echo "subjids    list of subject IDs"
     echo "runids     list of run IDs"
     echo
     echo "Level 1 Template variables:"
@@ -18,18 +17,20 @@ then
     echo "SUBJID     full subject identifier, e.g. prex_01"
     echo "RUNID      full identifier for each run, e.g. pre_1"
     echo
+    echo "Environment variables used:"
+    echo "STUDYDIR   path to directory with subject directories"
+    echo "SUBJIDS    list of subject identifiers"
+    echo
     exit 1
 fi
 
 template=$1
-studydir=$2
-model=$3
-subjids="$4"
-runids="$5"
+model=$2
+runids="$3"
 
 # study-level directory for automatically generated scripts and
 # later higher-level analysis
-glmdir=${studydir}/batch/glm
+glmdir=${STUDYDIR}/batch/glm
 modeldir=${glmdir}/${model}
 fsfdir=${modeldir}/fsf
 mkdir -p ${fsfdir}
@@ -37,13 +38,13 @@ mkdir -p ${fsfdir}
 fsftemplate=${modeldir}/${model}_level1.fsf
 cp ${template} ${fsftemplate}
 
-for subjid in ${subjids}; do
+for subjid in ${SUBJIDS}; do
     for runid in ${runids}; do
 	# path to customized FSF file
 	customfsf=${fsfdir}/${model}_${subjid}_${runid}.fsf
 
 	# use sed to create the customized file
-	sed -e "s|STUDYDIR|${studydir}|g" \
+	sed -e "s|STUDYDIR|${STUDYDIR}|g" \
 	    -e "s|SUBJID|${subjid}|g" \
 	    -e "s|RUNID|${runid}|g" \
 	    <${fsftemplate} >${customfsf}
