4ccbf84487fe74b6741e4e0e68864e0c80560e1e
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-02 12:32:50 -0500

    check_func_reg.sh: changed to match new organization

--- preproc/check_func_reg.sh
+++ preproc/check_func_reg.sh
@@ -17,18 +17,15 @@ CHECKDIR=$BOLDDIR/antsreg/checks
 mkdir -p $CHECKDIR
 
 # functional volume all others are aligned to
-REFVOL=${BOLDDIR}/${RUNCODE}_${REFRUN}/bold_mcf_brain_vol1.nii.gz
+REFVOL=${BOLDDIR}/antsreg/data/refvol.nii.gz
 
 FILES=""
-for r in $RUNS
-do
-    if [ r != $REFRUN ]
-    then
+for r in $RUNS; do
+    if [ r != $REFRUN ]; then
 	# get first volume from the aligned time series
-	RUNTS=${BOLDDIR}/antsreg/data/${RUNCODE}_${r}_bold_mcf_brain.nii.gz
-	RUNVOL1=${BOLDDIR}/antsreg/data/${RUNCODE}_${r}_bold_mcf_brain_vol1.nii.gz
-	if [ -e $RUNTS ]
-	then
+	RUNTS=${BOLDDIR}/antsreg/data/${RUNCODE}_${r}.nii.gz
+	RUNVOL1=${BOLDDIR}/antsreg/data/${RUNCODE}_${r}_vol1.nii.gz
+	if [ -e $RUNTS ]; then
 	    fslroi $RUNTS $RUNVOL1 0 1
 
 	    # create slice images and concatenate them horizontally
@@ -41,8 +38,7 @@ done
 
 # create an image with all runs
 C=""
-for f in $FILES
-do
+for f in $FILES; do
     C="$C - $f"
 done
 
