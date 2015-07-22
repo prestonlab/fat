06ba81c87c50cdd3ca8d1619d8608481c78c819e
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-22 12:26:28 -0500

    convert_freesurfer.py: added brain mask creation from fs parcelation

--- preproc/convert_freesurfer.py
+++ preproc/convert_freesurfer.py
@@ -18,7 +18,7 @@ if not os.path.exists(src):
 
 log.start()
 src_names = ['orig', 'brainmask', 'aparc+aseg', 'wm']
-dest_names = ['orig', 'orig_brain', 'aparc+aseg', 'wm']
+dest_names = ['orig', 'orig_brain_auto', 'aparc+aseg', 'wm']
 for i in range(len(src_names)):
     src_file = os.path.join(src, src_names[i] + '.mgz')
     dest_file = os.path.join(dest, dest_names[i] + '.nii.gz')
@@ -32,4 +32,18 @@ for i in range(len(src_names)):
 
     # fix orientation
     log.run('fslreorient2std %s %s' % (dest_file, dest_file))
+
+# use the FS parcelation to get an improved brain extraction
+
+# smooth and threshold the identified tissues; fill any remaining holes
+parcel = impath(dest, 'aparc+aseg')
+mask = impath(dest, 'brainmask')
+log.run('fslmaths %s -thr 0.5 -bin -s 0.25 -bin -fillh26 %s' % (
+    parcel, mask))
+
+# create a brain-extracted image
+fs_brain = impath(dest, 'orig_brain_auto')
+output = impath(dest, 'orig_brain')
+log.run('fslmaths %s -mul %s %s' % (mask, fs_brain, output))
+
 log.finish()
