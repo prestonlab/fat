3c224f0afa1cbe87240f557e6a254aaf37b12831
Neal Morton <neal.morton@austin.utexas.edu>
2015-07-28 14:19:59 -0500

    submit_run_job.sh: updated docstring; changed inputs to match submit_subj_job order

--- utils/submit_run_job.sh
+++ utils/submit_run_job.sh
@@ -4,6 +4,7 @@ projname=ANTS
 queue=normal
 compiler=gcc
 parenv=12way
+ncores=12
 
 OPTIND=1
 while getopts ":j:q:c:r:e:p:" opt; do
@@ -26,34 +27,35 @@ done
 shift $((OPTIND-1))
 
 if [ $# -lt 1 ]; then
-    echo "submit_run_job.sh   Submits jobs for multiple subjects."
+    echo "submit_run_job.sh   Submits a job to process multiple runs."
     echo
-    echo "Usage: submit_run_job.sh [-jqcrep] runids commands"
+    echo "Usage: submit_run_job.sh [-jqcrep] commands runids"
     echo "See launch for explanation of options."
     echo
     echo "In the commands string, any '{}' will be replaced with"
-    echo "run identifier. Takes subject numbers (e.g. 1, 2)"
-    echo "and constructs them in the format [study]_DD, e.g. bender_01."
-    echo "If the environment variable SUBJNOS is set, that will"
-    echo "be used to set the subjects list."
+    echo "run identifier. Can also use for replacing other simple"
+    echo "variables in a command, such as COPE number."
     echo
     echo "Example (export commands can be placed in your .bashrc):"
     echo "export STUDY=bender"
-    echo 'export SUBJNOS=`seq 1 6`'
-    echo 'submit_subj_job.sh -r 01:00:00 "convert_dicom.py {s} {r}"'
+    echo 'submit_run_job.sh -r 01:00:00 "run_level3.sh loc_cond {}" "1 2 3 4"'
     exit 1
 fi
 
-runids="$1"
+#echo "project=$projname queue=$queue compiler=$compiler parenv=$parenv runtime=$runtime ncores=$ncores"
+
+runids="$2"
 
 jobfile=`get_auto_jobfile.sh`
-command="$2"
+command="$1"
 
+echo "Creating file: $jobfile"
 for runid in $runids; do
     run_command=`echo $command | sed s/{}/$runid/g`
     echo $run_command >> $jobfile
     echo "$run_command"
 done
+
 chmod +x $jobfile
 
 cd `dirname $jobfile`
