2149fafcbfb903d6f249f62c4ebeb0b0983515ea
Neal Morton <neal.morton@austin.utexas.edu>
2015-08-24 22:22:29 -0500

    anat_rois.py: added option of subdir to look in

--- anatomy/anat_rois.py
+++ anatomy/anat_rois.py
@@ -3,6 +3,7 @@
 from subjutil import *
 
 parser = SubjParser()
+parser.add_argument('--reg', '-r', help='registration type', default='bbreg')
 args = parser.parse_args()
 
 sp = SubjPath(args.subject)
@@ -10,7 +11,7 @@ log = SubjLog(args.subject, 'anatrois', 'preproc',
               args.clean_logs, args.dry_run)
 
 log.start()
-data_dir = sp.path('anatomy', 'bbreg', 'data')
+data_dir = sp.path('anatomy', args.reg, 'data')
 parc_file = impath(data_dir, 'aparc+aseg')
 log.run('roi_freesurfer.sh %s %s' % (parc_file, data_dir))
 log.finish()
