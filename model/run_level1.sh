58fd0a46d563a4ad780f2110310f670b4291f63c
Neal Morton <neal.morton@austin.utexas.edu>
2015-08-24 22:27:12 -0500

    run_level1.sh: added check for empty onset files

--- model/run_level1.sh
+++ model/run_level1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 3 ]; then
-    echo "Usage: run_level1.sh model subjid runid"
+    echo "Usage:   run_level1.sh model subjid runid"
     echo "Example: run_level1.sh prex_cond bender_01 loc_1"
     exit 1
 fi
@@ -13,12 +13,24 @@ runid=$3
 glmdir=${STUDYDIR}/batch/glm
 modeldir=${glmdir}/${model}
 
+# get the FSF file for this subject and run
 fsf=${STUDYDIR}/batch/glm/${model}/fsf/${model}_${subjid}_${runid}.fsf
 if [ ! -e $fsf ]; then
     echo "FSF file not found: ${fsf}. Exiting."
     exit 1
 fi
 
+# determine whether any of the onset files are empty; if they are,
+# then Feat won't be able to run, so just exit
+onset_files=`cat $fsf | grep 'set fmri(custom[0-9]*) \".*.txt\"' | cut -d \" -f 2`
+for file in $onset_files; do
+    if [ ! -s $file ]; then
+	echo "Onset file is empty: $file. Cannot run Feat. Exiting."
+	exit 2
+    fi
+done
+
+# prepare the subject's model directory
 subjmodeldir=${STUDYDIR}/${subjid}/model/${model}
 mkdir -p $subjmodeldir
 rm -rf $subjmodeldir/${runid}*.feat
@@ -28,4 +40,6 @@ cd ${STUDYDIR}
 # unset the SGE_ROOT variable. This indicates to FSL that it should
 # not attempt to submit jobs
 export SGE_ROOT=""
+
+# run Feat
 feat ${fsf}
