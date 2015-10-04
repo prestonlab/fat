1c35261c462f56dde0d29ab1c42d866ef7c89896
Neal Morton <neal.morton@austin.utexas.edu>
2015-10-03 19:52:25 -0500

    unwarp_bold.py: updated implementation

--- preproc/unwarp_bold.py
+++ preproc/unwarp_bold.py
@@ -25,17 +25,16 @@ log.start()
 run_dirs = sp.bold(args.run_pattern)
 
 for i in range(len(run_dirs)):
-    bold = os.path.join(run_dirs[i], 'bold_mcf_brain_avg.nii.gz')
+    bold = impath(run_dirs[i], 'bold_mcf_brain_avg')
     fm_dir = os.path.join(run_dirs[i], 'fm')
-    fm = os.path.join(fm_dir, 'epireg_fieldmaprads2epi.nii.gz')
-    fm_mask = os.path.join(fm_dir,
-                           'epireg_fieldmaprads2epi_mask.nii.gz')
+    fm = impath(fm_dir, 'epireg_fieldmaprads2epi')
+    fm_mask = impath(fm_dir, 'epireg_fieldmaprads2epi_mask')
     log.run('fslmaths %s -abs -bin %s' % (fm, fm_mask))
 
     # calculate the voxel shift and unwarp the average run image for
     # registraion purposes
-    output = os.path.join(run_dirs[i], 'bold_mcf_brain_avg_unwarp.nii.gz')
-    shift = os.path.join(fm_dir, 'epireg_fieldmaprads2epi_sm_shift.nii.gz')
+    output = impath(run_dirs[i], 'bold_mcf_brain_avg_unwarp')
+    shift = impath(fm_dir, 'epireg_fieldmaprads2epi_sm_shift')
     cmd = 'fugue -i %s --loadfmap=%s --mask=%s --dwell=%.6f --unwarpdir=%s --smooth3=%.6f -u %s --saveshift=%s --unmaskshift' % (
         bold, fm, fm_mask, args.echo_spacing, args.pedir, args.smooth3,
         output, shift)
