7867627c8d47c08d46a32418feb90f95ce324294
Neal Morton <mortonne@gmail.com>
2016-04-15 13:49:46 -0500

    change to use more robust access to slurm data

--- utils/running_jobs.sh
+++ utils/running_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 job_dir=${BATCHDIR}
-running=`squeue -u $USER | grep Job | awk '{print $3}'`
+running=`squeue -u $USER -h -o "%j"`
 for f in $running; do
     file=${job_dir}/${f}.sh
     echo "$f:"
@@ -9,4 +9,3 @@ for f in $running; do
 	echo $line
     done < $file
 done
-
