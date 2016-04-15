12ad242552cf61539ed4f14db7b3207d0696d134
Neal Morton <mortonne@gmail.com>
2016-04-15 13:52:16 -0500

    add support for running multiple commands per subject

--- utils/submit_subj_job.sh
+++ utils/submit_subj_job.sh
@@ -10,9 +10,21 @@ if [ $# -lt 2 ]; then
     echo "subject identifier. Takes subject numbers (e.g. 1, 2)"
     echo "and constructs them in the format [study]_DD, e.g. bender_01."
     echo
-    echo "Example (export command can be placed in your .bashrc):"
+    echo "Example 1:"
     echo "export STUDY=bender"
-    echo 'submit_subj_job.sh "convert_dicom.py {}" "1 2 3 4" -r 01:00:00'
+    echo 'submit_subj_job.sh "convert_dicom.py {}" "1 2 3 4" -r 01:00:00 -e 4'
+    echo "runs convert_dicom.py bender_01, convert_dicom.py bender_02, ..."
+    echo "running 4 subjects per node, with 1 hour allocated to the job."
+    echo "The export command can be placed in your .bashrc."
+    echo
+    echo "Example 2:"
+    echo 'submit_subj_job.sh "echo {};echo{}-2" "1 2 3 4" -r 01:00:00'
+    echo "Can separate different commands to be run in parallel for a given"
+    echo "subject with ';'. Here, will run e.g. 'echo bender_01' and"
+    echo "'echo bender_01-2' in parallel. This can be used to batch"
+    echo "multiple commands per subject in a single job. To run commands"
+    echo "in serial instead, write a script that runs multiple commands"
+    echo "and call that."
     exit 1
 fi
 
@@ -26,8 +38,10 @@ n=0
 for no in $nos; do
     n=$((n + 1))
     subject=${STUDY}_`printf "%02d" $no`
-    subj_command=`echo $command | sed s/{}/$subject/g`
-    echo $subj_command >> $jobfile
+
+    # fill in subject ID and split commands
+    subj_command=`echo $command | sed s/{}/$subject/g | tr ';' '\n'`
+    echo -e "$subj_command" >> $jobfile
     echo "$subj_command"
 done
 
