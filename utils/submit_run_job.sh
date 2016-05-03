4528e7e4593e88013bc855e1e375836450d0e902
Neal Morton <mortonne@gmail.com>
2016-05-03 03:08:20 -0500

    update to colon-separated lists

--- utils/submit_run_job.sh
+++ utils/submit_run_job.sh
@@ -8,11 +8,15 @@ if [ $# -lt 1 ]; then
     echo
     echo "In the commands string, any '{}' will be replaced with"
     echo "run identifier. Can also use for replacing other simple"
-    echo "variables in a command, such as COPE number."
+    echo "variables in a command, such as COPE number. Run IDs"
+    echo "must be a list separated by colons."
     echo
-    echo "Example (export commands can be placed in your .bashrc):"
-    echo "export STUDY=bender"
-    echo 'submit_run_job.sh -r 01:00:00 "run_level3.sh loc_cond {}" "1 2 3 4"'
+    echo "May include multiple commands to run in parallel for each"
+    echo "run by separating commands with a colon; each of the"
+    echo "commands will be run in parallel."
+    echo
+    echo "Example:"
+    echo 'submit_run_job.sh "run_level3.sh loc_cond {}" 1:2:3:4 -r 01:00:00'
     exit 1
 fi
 
@@ -20,20 +24,20 @@ command="$1"
 runids="$2"
 shift 2
 
-jobfile=`get_auto_jobfile.sh`
+runs=$(echo $runids | sed "s/:/ /g")
+jobfile=$(get_auto_jobfile.sh)
 
 echo "Creating file: $jobfile"
-for runid in $runids; do
-    run_command=`echo $command | sed s/{}/$runid/g`
+for runid in $runs; do
+    run_command=$(echo $command | sed s/{}/$runid/g | tr ':' '\n')
     echo $run_command >> $jobfile
     echo "$run_command"
 done
 
 chmod +x $jobfile
 
-cd `dirname $jobfile`
-file=`basename $jobfile`
-name=`echo $file | cut -d . -f 1`
-launch -s $file -j $projname -q $queue -c $compiler -n $name \
-       -e $parenv -r $runtime -p $ncores
+cd $(dirname $jobfile)
+file=$(basename $jobfile)
+name=$(echo $file | cut -d . -f 1)
 
+launch -s $jobfile -J $name "$@"
