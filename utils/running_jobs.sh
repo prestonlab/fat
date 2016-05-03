f99608b63b7ad9f9b83b5e09ec3c4a3c182c375f
Neal Morton <mortonne@gmail.com>
2016-05-03 03:07:21 -0500

    add more job information

--- utils/running_jobs.sh
+++ utils/running_jobs.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 job_dir=${BATCHDIR}
-running=`squeue -u $USER -h -o "%j"`
+running=`squeue -u $USER -h -o "%j" -S i`
 for f in $running; do
-    file=${job_dir}/${f}.sh
-    echo "$f:"
-    while read line; do
-	echo $line
-    done < $file
+    status=`squeue -u $USER -n $f -h -o %T`
+    elapsed=`squeue -u $USER -n $f -h -o %M`
+    limit=`squeue -u $USER -n $f -h -o %l`
+    file=${job_dir}/${f}
+    if [ -e ${file}.sh ]; then
+	ext=.sh
+    else
+	ext=.txt
+    fi
+	
+    if [ -e ${file}${ext} ]; then
+	echo "$f ($status $elapsed/$limit):"
+	while read line; do
+	    echo $line
+	done < ${file}${ext}
+    else
+	echo "$f ($status $elapsed/$limit)"
+    fi
 done
