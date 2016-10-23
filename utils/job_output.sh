995d65ad32f34a75712e7473b20f18bcb8328d88
Neal Morton <mortonne@gmail.com>
2016-10-23 15:33:13 -0500

    add optional job name input

--- utils/job_output.sh
+++ utils/job_output.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 job_dir=${BATCHDIR}
-running=`squeue -u $USER -h -o "%j"`
+
+if [ $# -lt 1 ]; then
+    running=`squeue -u $USER -h -o "%j"`
+else
+    running="$1"
+fi
+shopt -s nullglob
 for f in $running; do
-    file=`ls -1 ${job_dir}/${f}.o*`
-    echo "$f output:"
-    while read line; do
-	echo $line
-    done < $file
+    for file in ${job_dir}/${f}.o*; do
+	echo "$f output:"
+	while read line; do
+	    echo $line
+	done < $file
+    done
 done
