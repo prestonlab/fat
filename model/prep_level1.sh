fd435392cbaf105dfc723f1b6b2a893d1a2d038d
Neal Morton <mortonne@gmail.com>
2016-10-24 17:51:49 -0500

    add ability to take subjids as an input

--- model/prep_level1.sh
+++ model/prep_level1.sh
@@ -5,12 +5,14 @@ then
     echo "Prepare level 1 analysis from an FSL template"
     echo
     echo "Usage:"
-    echo "`basename $0` [template] [model] [runids]"
+    echo "`basename $0` template model runids [subjids]"
     echo
     echo "Inputs:"
     echo "template   path to FSF template"
     echo "model      name of statistical model"
-    echo "runids     list of run IDs"
+    echo "runids     list of run IDs (separated by :)"
+    echo "subjids    list of subject IDs. If not specified,"
+    echo "           SUBJIDS environment variable will be used."
     echo
     echo "Level 1 Template variables:"
     echo "STUDYDIR   path to directory with study data"
@@ -19,7 +21,7 @@ then
     echo
     echo "Environment variables used:"
     echo "STUDYDIR   path to directory with subject directories"
-    echo "SUBJIDS    list of subject identifiers"
+    echo "SUBJIDS    (optional) list of subject identifiers"
     echo
     exit 1
 fi
@@ -27,6 +29,15 @@ fi
 template=$1
 model=$2
 runids="$3"
+if [ $# -lt 4 ]; then
+    if [ -u $SUBJIDS ]; then
+	echo "ERROR: Must specify subject identifiers."
+	exit 1
+    fi
+    sids="`echo $SUBJIDS | sed "s/:/ /g"`"
+else
+    sids="`echo $4 | sed "s/:/ /g"`"
+fi
 
 # study-level directory for automatically generated scripts and
 # later higher-level analysis
@@ -38,7 +49,6 @@ mkdir -p ${fsfdir}
 fsftemplate=${modeldir}/${model}_level1.fsf
 cp ${template} ${fsftemplate}
 
-sids="`echo $SUBJIDS | sed "s/:/ /g"`"
 rids="`echo $runids | sed "s/:/ /g"`"
 for subjid in $sids; do
     for runid in $rids; do
