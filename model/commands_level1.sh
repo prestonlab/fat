cbc80d86c08e376412a6451fb44437676b609ba0
Neal Morton <mortonne@gmail.com>
2016-10-24 17:52:16 -0500

    add usage info, style fixes

--- model/commands_level1.sh
+++ model/commands_level1.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    echo "Print commands to run a level 1 model for all scans."
+    echo "Usage:   commands_level1.sh model runids"
+    echo "Example: commands_level1.sh loc_subcat loc_1:loc_2:loc_3:loc_4"
+    exit 1
+fi
+
 model=$1
 runids=$2
 
-sids="`echo $SUBJNOS | sed "s/:/ /g"`"
-rids="`echo $runids | sed "s/:/ /g"`"
+sids="$(echo $SUBJNOS | sed "s/:/ /g")"
+rids="$(echo $runids | sed "s/:/ /g")"
 for no in $sids; do
-    subjid=`subjids $no`
+    subjid=$(subjids $no)
     for runid in $rids; do
 	dof_file=$STUDYDIR/$subjid/model/$model/${runid}.feat/stats/dof
 	if [ ! -e $dof_file ]; then
