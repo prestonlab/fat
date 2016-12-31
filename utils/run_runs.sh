67e964d7aad3390fdd5a4c2959a3080308578f58
Neal Morton <mortonne@gmail.com>
2016-12-31 15:17:01 -0600

    make more robust for when only run list is specified

--- utils/run_runs.sh
+++ utils/run_runs.sh
@@ -3,7 +3,7 @@
 if [ $# -lt 1 ]; then
     echo "run_runs.sh   Run a command on multiple subjects and runs."
     echo
-    echo "Usage: run_runs.sh commands [subjects] [runs]"
+    echo "Usage: run_runs.sh commands runs [subjects]"
     echo
     echo "In the commands string, any '{s}' will be replaced with"
     echo "subject identifier. Takes subject numbers (e.g. 1, 2)"
@@ -35,28 +35,17 @@ shift $((OPTIND-1))
 command="$1"
 shift 1
 
+runs=`echo $1 | sed "s/:/ /g"`
+
 if [ $# -lt 2 ]; then
-    nos="$SUBJNOS"
+    nos=""
 else
     nos="$2"
 fi
 
 if [ -z "$nos" ]; then
-    echo "Error: must indicate subject numbers to include."
-    exit 1
-fi
-
-nos=`echo $nos | sed "s/:/ /g"`
-runs=`echo $1 | sed "s/:/ /g"`
-for no in $nos; do
-    if [ $ids == 1 ]; then
-	subject=$no
-    else
-	subject=${STUDY}_`printf "%02d" $no`
-    fi
-    subj_command=`echo $command | sed s/{s}/$subject/g`
     for run in $runs; do
-	run_command=`echo $subj_command | sed s/{r}/$run/g`
+	run_command=`echo $command | sed s/{r}/$run/g`
 	if [ $verbose -eq 1 ]; then
 	    echo "$run_command"
 	fi
@@ -64,4 +53,23 @@ for no in $nos; do
 	    eval $run_command
 	fi
     done
-done
+else
+    nos=`echo $nos | sed "s/:/ /g"`
+    for no in $nos; do
+	if [ $ids == 1 ]; then
+	    subject=$no
+	else
+	    subject=${STUDY}_`printf "%02d" $no`
+	fi
+	subj_command=`echo $command | sed s/{s}/$subject/g`
+	for run in $runs; do
+	    run_command=`echo $subj_command | sed s/{r}/$run/g`
+	    if [ $verbose -eq 1 ]; then
+		echo "$run_command"
+	    fi
+	    if [ $noexec -ne 1 ]; then
+		eval $run_command
+	    fi
+	done
+    done
+fi
