26c2460ca1766ba6ad5538836dfcdc53fe82ce45
Neal Morton <mortonne@gmail.com>
2017-01-25 12:17:17 -0600

    minor fixes for brain mask, white matter mask

--- preproc/convert_freesurfer.py
+++ preproc/convert_freesurfer.py
@@ -16,8 +16,8 @@ if not os.path.exists(src):
     raise IOError('FreeSurfer directory does not exist: %s' % src)
 
 log.start()
-src_names = ['orig', 'brainmask', 'aparc+aseg', 'wm']
-dest_names = ['orig', 'orig_brain_auto', 'aparc+aseg', 'wm']
+src_names = ['orig', 'brainmask', 'aparc+aseg']
+dest_names = ['orig', 'orig_brain_auto', 'aparc+aseg']
 for i in range(len(src_names)):
     src_file = os.path.join(src, src_names[i] + '.mgz')
     dest_file = os.path.join(dest, dest_names[i] + '.nii.gz')
@@ -41,8 +41,15 @@ log.run('fslmaths %s -thr 0.5 -bin -s 0.25 -bin -fillh26 %s' % (
     parcel, mask))
 
 # create a brain-extracted image
-fs_brain = impath(dest, 'orig_brain_auto')
 output = impath(dest, 'orig_brain')
-log.run('fslmaths %s -mul %s %s' % (mask, fs_brain, output))
+fs_brain = impath(dest, 'orig_brain_auto')
+log.run('fslmaths %s -mas %s %s' % (fs_brain, mask, output))
+log.run('fslmaths %s -thr 0.5 -bin %s' % (output, mask))
+
+# get white matter
+log.run('fslmaths %s -thr 2 -uthr 2 %s' % (parcel, impath(dest, 'l_wm')))
+log.run('fslmaths %s -thr 41 -uthr 41 %s' % (parcel, impath(dest, 'r_wm')))
+log.run('fslmaths %s -add %s -bin %s' % (
+    impath(dest, 'l_wm'), impath(dest, 'r_wm'), impath(dest, 'wm')))
 
 log.finish()
