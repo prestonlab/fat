b102cf053c8a23e5321aa8002529c35f236bdb73
Neal Morton <mortonne@gmail.com>
2017-01-25 12:18:13 -0600

    add option for setting nprocs, rescale intensities for equal weighting

--- preproc/merge_anat.sh
+++ preproc/merge_anat.sh
@@ -7,24 +7,24 @@ if [ $# -lt 3 ]; then
     echo
     echo "will create a temporary directory called ${image1}_${image2}"
     echo "with intermediate images."
-    echo "-c"
-    echo "    run bias field correction on each image before registration"
     echo "-b"
     echo "    run brain extraction on each image before registration."
     echo "    Final merged image will include the full head, but only"
     echo "    the brain is used for registration."
+    echo "-n"
+    echo "    number of threads to use for registration (default 1)."
     exit 1
 fi
 
+nthreads=1
 bet=0
-cor=0
-while getopts ':bc' opt; do
+while getopts ':bcn:' opt; do
     case $opt in
 	b)
 	    bet=1
 	    ;;
-	c)
-	    cor=1
+	n)
+	    nthreads=$OPTARG
 	    ;;
     esac
 done
@@ -34,6 +34,8 @@ shift $((OPTIND-1))
 im1=$1
 im2=$2
 out=$3
+
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=$nthreads
 
 name1=$(basename $im1 | cut -d . -f 1)
 name2=$(basename $im2 | cut -d . -f 1)
@@ -50,13 +52,11 @@ im1=im1
 im2=im2
 
 # correct image intensity
-if [ $cor = 1 ]; then
-    echo "Intensity normalization..."
-    N4BiasFieldCorrection -i ${im1}.nii.gz -o ${im1}_cor.nii.gz
-    N4BiasFieldCorrection -i ${im2}.nii.gz -o ${im2}_cor.nii.gz
-    im1=${im1}_cor
-    im2=${im2}_cor
-fi
+echo "Intensity normalization..."
+N4BiasFieldCorrection -i ${im1}.nii.gz -o ${im1}_cor.nii.gz
+N4BiasFieldCorrection -i ${im2}.nii.gz -o ${im2}_cor.nii.gz
+im1=${im1}_cor
+im2=${im2}_cor
 
 # extract the brain
 if [ $bet = 1 ]; then
@@ -73,13 +73,26 @@ antsRegistration -d 3 -r [${im1}.nii.gz,${im2}.nii.gz,1] -t Rigid[0.1] -m MI[${i
 
 # apply transform to corrected, non-skull-stripped image
 echo "Applying transformation..."
-if [ $cor = 1 ]; then
-    antsApplyTransforms -i im2_cor.nii.gz -o im2_cor_reg.nii.gz -r im1_cor.nii.gz -t im2-im1_0GenericAffine.mat -n BSpline
-    fslmaths im1_cor -add im2_cor_reg -div 2 im_merge
-else
-    antsApplyTransforms -i im2.nii.gz -o im2_reg.nii.gz -r im1.nii.gz -t im2-im1_0GenericAffine.mat -n BSpline
-    fslmaths im1 -add im2_reg -div 2 im_merge
-fi
+antsApplyTransforms -i im2_cor.nii.gz -o im2_cor_reg.nii.gz -r im1_cor.nii.gz -t im2-im1_0GenericAffine.mat -n BSpline
+
+# calculate mask
+for file in im1_cor im2_cor_reg; do
+    int_2_98=$(fslstats $file -p 2 -p 98)
+    int2=$(echo $int_2_98 | awk '{print $1}')
+    int98=$(echo $int_2_98 | awk '{print $2}')
+    thresh=$(python -c "print $int2 + 0.1 * ($int98 - $int2)")
+    fslmaths $file -thr $thresh -bin ${file}_mask
+done
+fslmaths im1_cor_mask -mul im2_cor_reg_mask mask
+
+# normalize global intensity between images so they are equally weighted
+for file in im1_cor im2_cor_reg; do
+    fslmaths $file -mas mask ${file}_thresh
+    fslmaths ${file}_thresh -inm 1000 ${file}_norm
+done
+
+# average
+fslmaths im1_cor_norm -add im2_cor_reg_norm -div 2 im_merge
 
 # move merged image
 cd $pd
