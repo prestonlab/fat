85ad136b06eef3f821748fa0c801e5d04d141845
Neal Morton <mortonne@gmail.com>
2017-03-10 18:05:54 -0600

    script to merge hdf5 datasets

---
+++ utils/h5merge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+from mvpa2.base import hdf5
+from mvpa2.base.dataset import vstack
+
+outfile = sys.argv[1]
+
+# load and concatenate all input files
+for i, file in enumerate(sys.argv[2:]):
+    print file
+    if i == 0:
+        ds = hdf5.h5load(file)
+        ds.sa['chunks'] = [i]
+    else:
+        newds = hdf5.h5load(file)
+        newds.sa['chunks'] = [i]
+        ds = vstack((ds, newds))
+
+# save the full dataset
+ds.save(outfile)
