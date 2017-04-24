df743083ac80648e84479d580eb9aa61ffb123dc
Neal Morton <mortonne@gmail.com>
2017-04-24 17:47:15 -0500

    option to allow partial sets of level 1 models

--- model/prep_level2.sh
+++ model/prep_level2.sh
@@ -5,7 +5,7 @@ then
     echo "Prepare level 2 analysis from an FSL template"
     echo
     echo "Usage:"
-    echo "`basename $0` [-ds] template model"
+    echo "`basename $0` [-d studydir] [-s subjids] [-p] template model"
     echo
     echo "Optional inputs:"
     echo "-d"
@@ -16,24 +16,38 @@ then
     echo "      list of subject ids, separated by ':'. If not"
     echo "      specified, SUBJIDS environment variable will be used."
     echo
+    echo "-p"
+    echo "      if set, will allow use of a partial set of level 1 models."
+    echo "      Will check for existence of a feat directory for each"
+    echo "      run in each subject's fsf file. If all are missing, no"
+    echo "      level 2 fsf will be generated. If some are missing, each"
+    echo "      level 2 fsf file will just use the level 1 feat directories"
+    echo "      that exist."
+    echo
     echo "Required inputs:"
     echo "template   path to FSF template"
     echo "model      name of statistical model"
     echo
     echo "These strings will be replaced in the FSF template:"
     echo "STUDYDIR   path to directory with study data"
-    echo "SUBJID     full subject identifier, e.g. prex_01"
+    echo "SUBJID     full subject identifier, e.g. bender_01"
+    echo
+    echo 'Customized fsf files will be placed in $STUDYDIR/batch/glm/$model.'
     echo
     exit 1
 fi
 
-while getopts ":d:s:" opt; do
+partial=0
+while getopts ":d:s:p" opt; do
     case $opt in
 	d)
 	    STUDYDIR=$OPTARG
 	    ;;
 	s)
 	    SUBJIDS=$OPTARG
+	    ;;
+	p)
+	    partial=1
 	    ;;
     esac
 done
@@ -65,4 +79,28 @@ for subjid in ${sids}; do
     sed -e "s|STUDYDIR|${STUDYDIR}|g" \
 	-e "s|SUBJID|${subjid}|g" \
 	<${fsftemplate} >${customfsf}
+
+    if [ $partial = 1 ]; then
+	# get inputs that exist
+	files=$(grep feat_files $customfsf | cut -d '"' -f 2 | tr '\n' ' ')
+	include=""
+	for f in $files; do
+	    if [ -e $f ]; then
+		if [ -z "$include" ]; then
+		    include=$f
+		else
+		    include="$include $f"
+		fi
+	    fi
+	done
+	
+	if [ -z "$include" ]; then
+	    # no inputs exist; remove fsf file
+	    rm $customfsf
+	else
+	    # some inputs exist; create custom fsf file
+	    gfeat_subset $customfsf temp $include
+	    mv temp $customfsf
+	fi
+    fi
 done
