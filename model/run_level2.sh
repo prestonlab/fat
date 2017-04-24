510374df82400c357d69e52385321194fa74f7b9
Neal Morton <mortonne@gmail.com>
2017-04-24 17:48:07 -0500

    clarify usage

--- model/run_level2.sh
+++ model/run_level2.sh
@@ -3,6 +3,12 @@
 if [ $# -lt 2 ]; then
     echo "Usage:   run_level2.sh model subjid"
     echo "Example: run_level2.sh loc_cond bender_01"
+    echo
+    echo "Assumes that the fsf file is in:"
+    echo '$STUDYDIR/batch/glm/$model/fsf/${model}_${subjid}.fsf'
+    echo
+    echo "Any existing gfeat directories at the outputdir path will"
+    echo "be overwritten."
     exit 1
 fi
 
@@ -10,7 +16,7 @@ model=$1
 subjid=$2
 
 if [ -z $STUDYDIR ]; then
-    echo "Error: study directory is undefined: $STUDYDIR" 1>&2
+    echo "Error: STUDYDIR is undefined." 1>&2
     exit 1
 fi
 
@@ -20,10 +26,12 @@ if [ ! -e $fsf ]; then
     exit 1
 fi
 
-model_dir=${STUDYDIR}/${subjid}/model/${model}
+# get the output directory from the fsf file
 output_dir=$(grep outputdir $fsf | awk -F '"' '{ print $2 }')
 parent_dir=$(dirname $output_dir)
 output_name=$(basename $output_dir)
+
+# remove existing output directories
 if cd $parent_dir; then
     rm -rf $output_name*.gfeat
 else
