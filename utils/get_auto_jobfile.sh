9f546552fea5cc5419d9bf53f8f9d8381242a85e
Neal Morton <mortonne@gmail.com>
2017-05-24 14:11:39 -0500

    fix for when other script files are present

--- utils/get_auto_jobfile.sh
+++ utils/get_auto_jobfile.sh
@@ -7,8 +7,7 @@ cd $BATCHDIR
 if [ ! -e Job1.sh ]; then
     file=Job1.sh
 else
-    files=`ls Job*.sh`
-    ar=(`ls *.sh -1 | cut -c 4- | cut -d . -f 1`)
+    ar=(`ls Job*.sh -1 | cut -c 4- | cut -d . -f 1`)
 
     max=0
     for n in "${ar[@]}" ; do
