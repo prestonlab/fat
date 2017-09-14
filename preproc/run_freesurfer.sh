1be1c89756e3afb6251e7303a194e9ba2f85ff6d
Neal Morton <mortonne@gmail.com>
2017-09-14 18:29:15 -0500

    add support for using flair images, customizing the name of the output directory

--- preproc/run_freesurfer.sh
+++ preproc/run_freesurfer.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "Usage: run_freesurfer.sh subject nthreads"
+    echo "Usage: run_freesurfer.sh [-f] [-o outname] subject nthreads"
+    echo
+    echo "Use highres anatomical image to run FreeSurfer standard"
+    echo "reconstruction. Highres is expected to be in:"
+    echo '$STUDYDIR/$subject/anatomy/highres.nii.gz'
+    echo
+    echo "nthreads is the number of parallel threads to use per"
+    echo "hemisphere during processing."
+    echo
+    echo 'Results are saved in: $STUDYDIR/$subject/anatomy/$outname'
+    echo
+    echo "-f"
+    echo "    Use a FLAIR image to refine the pial surface. Expected to"
+    echo '    be in: $STUDYDIR/$subject/anatomy/flair.nii.gz'
+    echo
+    echo "-o outname"
+    echo '    Set the name of the output directory. Default is $subject.'
+    echo
     exit 1
 fi
 
@@ -15,9 +32,26 @@ if [ ! -d $STUDYDIR ]; then
     exit 1
 fi
 
+flair=false
+while getopts ":o:f" opt; do
+    case $opt in
+	o)
+	    outname=$OPTARG
+	    ;;
+	f)
+	    flair=true
+	    ;;
+    esac
+done
+shift $((OPTIND-1))
+
 subject=$1
 nthreads=$2
 subjdir=$STUDYDIR/$subject
+
+if [ -z $outname ]; then
+    outname=$subject
+fi
 
 if [ ! -f ${subjdir}/anatomy/highres.nii.gz ]; then
     echo "ERROR: Highres file not found."
@@ -25,12 +59,23 @@ if [ ! -f ${subjdir}/anatomy/highres.nii.gz ]; then
 fi
 
 # delete existing freesurfer results
-if [ -d ${subjdir}/anatomy/${subject} ]; then
+if [ -d ${subjdir}/anatomy/$outname ]; then
     cd ${subjdir}/anatomy
-    rm -rf ${subject}
+    rm -rf $outname
 fi
 
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-recon-all -s ${subject} -sd ${subjdir}/anatomy/ \
-	  -i ${subjdir}/anatomy/highres.nii.gz -all \
-	  -parallel -openmp $nthreads -itkthreads $nthreads 
+if [ $flair = true ]; then
+    if [ ! -e $subjdir/anatomy/flair.nii.gz ]; then
+	echo "Error: flair image not found."
+	exit 1
+    fi
+    recon-all -s ${outname} -sd ${subjdir}/anatomy/ \
+	      -i ${subjdir}/anatomy/highres.nii.gz -all \
+	      -FLAIR $subjdir/anatomy/flair.nii.gz -FLAIRpial \
+	      -parallel -openmp $nthreads -itkthreads $nthreads
+else
+    recon-all -s ${outname} -sd ${subjdir}/anatomy/ \
+	      -i ${subjdir}/anatomy/highres.nii.gz -all \
+	      -parallel -openmp $nthreads -itkthreads $nthreads
+fi
