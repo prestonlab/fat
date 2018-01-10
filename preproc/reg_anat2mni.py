3ed1e0c56714f37ac2e185226fbfe123da81b359
Neal Morton <mortonne@gmail.com>
2018-01-10 12:37:34 -0600

    add optional anatomical image number

--- preproc/reg_anat2mni.py
+++ preproc/reg_anat2mni.py
@@ -6,6 +6,8 @@ from subjutil import *
 
 parser = SubjParser()
 parser.add_argument('template', help="Path to template image")
+parser.add_argument('-a', '--anat', default='',
+    help="anatomical image number (default: none)")
 args = parser.parse_args()
 
 sp = SubjPath(args.subject, args.study_dir)
@@ -19,7 +21,7 @@ log.run('mkdir -p %s' % reg_data)
 log.run('mkdir -p %s' % reg_xfm)
 
 xfm_base = os.path.join(reg_xfm, 'orig-template_')
-highres = sp.image_path('anatomy', 'orig_brain')
+highres = sp.image_path('anatomy', 'orig_brain' + args.anat)
 
 # from buildtemplateparallel.sh
 log.run('ANTS 3 -m CC[{template},{highres},1,5] -t SyN[0.25] -r Gauss[3,0] -o {xfm} -i 30x90x20 --use-Histogram-Matching  --number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000'.format(
@@ -30,7 +32,10 @@ warp_file = xfm_base + 'Warp.nii.gz'
 images = ['orig', 'orig_brain', 'brainmask', 'aparc+aseg', 'wm', 'ctx']
 labels = [False, False, True, True, True, True]
 for i, image in enumerate(images):
-    image_file = sp.image_path('anatomy', image)
+    # add optional anatomical number to the input image name
+    image_file = sp.image_path('anatomy', image + args.anat)
+
+    # remove it from the destination image
     new_file = impath(reg_data, image)
     if labels[i]:
         cmd = 'WarpImageMultiTransform 3 %s %s -R %s --use-NN %s %s' % (
