7aaa9d697f785a513b020e4341fe27fd5790befc
Neal Morton <mortonne@gmail.com>
2018-02-19 20:09:15 -0600

    change to just run transform one level 1 model; add flag to indicate which anatomical image to use

--- model/transform_level1.py
+++ model/transform_level1.py
@@ -8,12 +8,10 @@ import warnings
 from subjutil import *
 
 parser = SubjParser()
-parser.add_argument('model', help="name of model")
+parser.add_argument('featdir', help="path to feat directory")
 parser.add_argument('template', help="path to template image")
-parser.add_argument('--feat-pattern', '-f',
-                    help="regular expression for feat directories",
-                    metavar='regexp',
-                    default='.*\.feat$')
+parser.add_argument('-a', '--anat', default='',
+                    help="anatomical image number used to register reference functional volume (default: none)")
 args = parser.parse_args()
 
 sp = SubjPath(args.subject, args.study_dir)
@@ -23,15 +21,9 @@ log = SubjLog(args.subject, 'translev1', 'model',
 bbreg = sp.path('anatomy', 'bbreg', 'transforms')
 antsreg = sp.path('anatomy', 'antsreg', 'transforms')
 
-t1_brain = sp.image_path('anatomy', 'orig_brain')
+t1_brain = sp.image_path('anatomy', 'orig_brain' + args.anat)
 t1_warped = sp.image_path('anatomy', 'antsreg', 'data', 'orig_brain')
 refvol = sp.image_path('bold', 'antsreg', 'data', 'refvol')
-
-# find all level 1 feat directories for this model
-feat_dirs = sp.feat_dirs(args.model)
-
-if len(feat_dirs) == 0:
-    sys.exit()
 
 log.start()
 
@@ -64,56 +56,56 @@ transform_label = Template('antsApplyTransforms -d 3 -i $native -o $mni -n Neare
 identity_affine = sp.proj_path('resources', 'identity.mat')
 images = ['example_func','mean_func','mask']
 islabel = [False,False,True]
-for feat in feat_dirs:
-    # prep directories for native-space images
-    native_dir = os.path.join(feat, 'native')
-    native_stats_dir = os.path.join(feat, 'native', 'stats')
-    if os.path.exists(native_dir):
-        continue
-    
-    if not os.path.exists(native_dir):
-        log.run('mkdir -p %s' % native_dir)
-    
-    # set up the reg directory for an identity transform (so that
-    # higher-level Feat doesn't change anything)
-    reg_dir = os.path.join(feat, 'reg')
-    affine_file = os.path.join(reg_dir, 'example_func2standard.mat')
-    standard_file = impath(reg_dir, 'standard')
-    highres_file = impath(reg_dir, 'highres')
-    log.run('mkdir -p %s' % reg_dir)
-    log.run('cp %s %s' % (identity_affine, affine_file))
-    log.run('cp %s %s' % (args.template, standard_file))
-    log.run('cp %s %s' % (t1_warped, highres_file))
 
-    # transform functional images
-    for i in range(len(images)):
-        native = impath(feat, native_dir, images[i])
-        mni = impath(feat, images[i])
-        if not os.path.exists(native):
-            log.run('mv %s %s' % (mni, native_dir))
-        
-        if islabel[i]:
-            log.run(transform_label.substitute(native=native, mni=mni))
-        else:
-            log.run(transform.substitute(native=native, mni=mni))
+# prep directories for native-space images
+native_dir = os.path.join(args.featdir, 'native')
+native_stats_dir = os.path.join(args.featdir, 'native', 'stats')
+if os.path.exists(native_dir):
+    continue
 
-    # move existing stats
-    stats_dir = os.path.join(feat, 'stats')
-    if not os.path.exists(native_stats_dir):
-        log.run('mv %s %s' % (stats_dir, native_dir))
-    if not os.path.exists(stats_dir):
-        log.run('mkdir -p %s' % stats_dir)
-    log.run('cp %s %s' % (os.path.join(native_stats_dir, 'dof'), stats_dir))
-    
-    # transform cope and varcope images
-    all_copes = glob(impath(native_stats_dir, '*cope*'))
-    for copefile in all_copes:
-        filename = os.path.basename(copefile)
-        native = impath(native_stats_dir, filename)
-        mni = impath(stats_dir, filename)
+if not os.path.exists(native_dir):
+    log.run('mkdir -p %s' % native_dir)
+
+# set up the reg directory for an identity transform (so that
+# higher-level Feat doesn't change anything)
+reg_dir = os.path.join(args.featdir, 'reg')
+affine_file = os.path.join(reg_dir, 'example_func2standard.mat')
+standard_file = impath(reg_dir, 'standard')
+highres_file = impath(reg_dir, 'highres')
+log.run('mkdir -p %s' % reg_dir)
+log.run('cp %s %s' % (identity_affine, affine_file))
+log.run('cp %s %s' % (args.template, standard_file))
+log.run('cp %s %s' % (t1_warped, highres_file))
+
+# transform functional images
+for i in range(len(images)):
+    native = impath(args.featdir, native_dir, images[i])
+    mni = impath(args.featdir, images[i])
+    if not os.path.exists(native):
+        log.run('mv %s %s' % (mni, native_dir))
+
+    if islabel[i]:
+        log.run(transform_label.substitute(native=native, mni=mni))
+    else:
         log.run(transform.substitute(native=native, mni=mni))
 
-    # prepare the new files for Feat
-    log.run('updatefeatreg %s -pngs' % feat)
+# move existing stats
+stats_dir = os.path.join(args.featdir, 'stats')
+if not os.path.exists(native_stats_dir):
+    log.run('mv %s %s' % (stats_dir, native_dir))
+if not os.path.exists(stats_dir):
+    log.run('mkdir -p %s' % stats_dir)
+    log.run('cp %s %s' % (os.path.join(native_stats_dir, 'dof'), stats_dir))
+
+# transform cope and varcope images
+all_copes = glob(impath(native_stats_dir, '*cope*'))
+for copefile in all_copes:
+    filename = os.path.basename(copefile)
+    native = impath(native_stats_dir, filename)
+    mni = impath(stats_dir, filename)
+    log.run(transform.substitute(native=native, mni=mni))
+
+# prepare the new files for Feat
+log.run('updatefeatreg %s -pngs' % args.featdir)
 
 log.finish()
