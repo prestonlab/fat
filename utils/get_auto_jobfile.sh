89c658c807b2e06621229078c0529ea80be22dd3
Neal Morton <mortonne@gmail.com>
2018-04-25 11:07:23 -0500

    add support for arbitrary basenames

--- utils/get_auto_jobfile.sh
+++ utils/get_auto_jobfile.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+#  get_auto_jobfile.sh   Generate a job file with a serial number.
+#
+#  Usage:
+#  get_auto_jobfile.sh [base]
+#
+#  If base is not specified, "Job" will be used. The file will be in BATCHDIR.
+
+if [ $# -lt 1 ]; then
+    base=Job
+else
+    base="$1"
+fi
+
 if [ -z $BATCHDIR ]; then
     echo "Error: Must define BATCHDIR to indicate directory to save jobs in." >&2
     exit 1
@@ -8,15 +22,15 @@ mkdir -p $BATCHDIR
 
 cd $BATCHDIR
 
-if [ ! -e Job1.sh ]; then
-    file=Job1.sh
+if [ ! -e "${base}"1.sh ]; then
+    file="${base}"1.sh
 else
-    ar=(`/bin/ls Job*.sh -1 | cut -c 4- | cut -d . -f 1`)
-
+    start=$((${#base}+1))
     max=0
-    for n in "${ar[@]}" ; do
+    for filename in "${base}"*.sh; do
+	n=$(echo $filename | cut -c ${start}- | cut -d . -f 1)
 	((n > max)) && max=$n
     done
-    file=Job$(( max + 1 )).sh
+    file="${base}"$(( max + 1 )).sh
 fi
 echo ${BATCHDIR}/${file}
