4e46aee500079aa2a406eda6853110a74ac751ea
Neal Morton <mortonne@gmail.com>
2018-04-25 15:26:02 -0500

    switched documentation to using a here document

--- utils/submit_job.sh
+++ utils/submit_job.sh
@@ -1,56 +1,51 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Usage: submit_job.sh [-x] [-J jobname] -s script [launch options]"
-    echo "Usage: submit_job.sh [-x] [-J jobname] commands [launch options]"
-    echo
-    echo "Use either -s script to indicate a file with commands to run,"
-    echo "where different lines should be run in parallel, or specify"
-    echo "commands to run."
-    echo
-    echo "-x"
-    echo "    Run remora on the command (only works with one command)"
-    echo "    to monitor resource usage (e.g. memory) periodically."
-    echo '    Results will be saved in $BATCHDIR/JobXXX.remora.'
-    echo
-    echo "-J"
-    echo "    Job name. Information about the job will be saved in"
-    echo "    $BATCHDIR/${jobname}XX.{sh,slurm,out}, where XX is a"
-    echo "    serial number. Default is: Job"
-    echo
-    echo "-s script"
-    echo '    Run commands in $script. Commands on different lines will'
-    echo "    be run in parallel. If this option is used, it must come"
-    echo "    last before any launch options."
-    echo
-    echo "Run launch -h for explanation of other options."
-    echo
-    echo "Must define environment variable BATCHDIR. For example:"
-    echo 'export BATCHDIR=$WORK/batch'
-    echo
-    echo "Jobs will be saved in BATCHDIR under Job1, Job2, etc."
-    echo "The command is placed in JobXXX.sh, output in JobXXX.out,"
-    echo "and slurm commands in JobXXX.slurm."
-    echo
-    echo "If jobname is specified, that will be used instead of JobXXX."
-    echo
-    echo "After submission, running_jobs.sh will show commands for"
-    echo "all jobs."
-    echo
-    echo "To look up how you ran a job to run some command before,"
-    echo 'run grep -l [command] $BATCHDIR/*.sh to find the job number.'
-    echo 'Then look at ${job}.out for duration, number of nodes, etc.'
-    echo
-    echo "Examples:"
-    echo "# Submit a job to display hello world:"
-    echo "submit_job.sh 'echo \"hello world\"' -N 1 -n 1 -r 00:01:00 -p development"
-    echo
-    echo "# Display hello world twice, executing in parallel on different"
-    echo "# cores of one node:"
-    echo "echo 'echo \"hello world 1\"' > myscript.sh"
-    echo "echo 'echo \"hello world 2\"' >> myscript.sh"
-    echo 'submit_job.sh -s myscript.sh -N 1 -n 2 -r 00:01:00 -p development'
-    echo
+    cat <<EOF
+Usage: submit_job.sh [-x] [-J jobname] -s script [launch options]
+Usage: submit_job.sh [-x] [-J jobname] commands [launch options]
+
+Use either -s script to indicate a file with commands to run,
+where different lines should be run in parallel, or specify
+commands to run.
+
+-x
+    Run remora on the command (only works with one command)
+    to monitor resource usage (e.g. memory) periodically.
+    Results will be saved in \$BATCHDIR/JobXXX.remora.
+
+-J
+    Job name. Information about the job will be saved in
+    \$BATCHDIR/\${jobname}XX.{sh,slurm,out}, where XX is a
+    serial number. Default is: Job
+
+-s script
+    Run commands in \$script. Commands on different lines will
+    be run in parallel. If this option is used, it must come
+    last before any launch options.
+
+Run launch -h for explanation of other options.
+
+Must define environment variable BATCHDIR. For example:
+export BATCHDIR=\$WORK/batch
+
+After submission, running_jobs.sh will show commands for
+all jobs.
+
+To look up how you ran a job to run some command before,
+run grep -l [command] \$BATCHDIR/*.sh to find the job name.
+Then look at \${job}.out for duration, number of nodes, etc.
+
+Examples:
+# Submit a job to display hello world:
+submit_job.sh 'echo "hello world"' -N 1 -n 1 -r 00:01:00 -p development
+
+# Display hello world twice, executing in parallel on different
+# cores of one node:
+echo 'echo "hello world 1"' > myscript.sh
+echo 'echo "hello world 2"' >> myscript.sh
+submit_job.sh -s myscript.sh -N 1 -n 2 -r 00:01:00 -p development
+EOF
     exit 1
 fi
 
