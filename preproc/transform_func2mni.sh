45033677d2c9163cba747e71eb686eb2fe7c2003
Neal Morton <mortonne@gmail.com>
2018-05-17 17:30:02 -0500

    update docstring format

--- preproc/transform_func2mni.sh
+++ preproc/transform_func2mni.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
 if [ $# -lt 3 ]; then
-    echo "Usage:   transform_func2mni.sh [-m] [-a anat] [-p postmask] input output subject"
-    echo "Example: transform_func2mni.sh bender_01_mat_item_w2v_faces.nii.gz bender_01_mat_item_w2v_faces_mni.nii.gz bender_01"
-    echo
-    echo "After running reg_anat2mni.py, can use this to transform any image"
-    echo "in functional space to template space (MNI or custom)."
-    echo
-    echo "input"
-    echo "    Path to an image in the space of the reference functional scan."
-    echo "    May be a single volume or a timeseries."
-    echo
-    echo "output"
-    echo "    Filename for image to be written in template space."
-    echo
-    echo "subject"
-    echo "    Subject code. Used to look up existing transforms. Must also"
-    echo "    set the STUDYDIR environment variable as the base directory"
-    echo "    for the study before calling."
-    echo
-    echo "OPTIONS"
-    echo "-a"
-    echo "    Suffix of anatomical image series that was used as reference"
-    echo "    When registration was done to the template. Should also be the"
-    echo "    image used as a target for registering the reference functional"
-    echo "    scan to the anatomical. If not specified, no suffix will be"
-    echo "    used."
-    echo
-    echo "-m"
-    echo "    Input image is a mask. If set, will use nearest neighbor"
-    echo "    interpolation. Otherwise, B-spline interpolation will be used."
-    echo
-    echo "-p"
-    echo "    Post-mask to apply to the transformed image. This is useful"
-    echo "    to remove very small values outside the brain that occur when"
-    echo "    using B-spline interpolation."
-    echo
+    cat <<EOF
+Usage:   transform_func2mni.sh [-m] [-a anat] [-p postmask] input output subject
+Example: transform_func2mni.sh bender_01_mat_item_w2v_faces.nii.gz bender_01_mat_item_w2v_faces_mni.nii.gz bender_01
+
+After running reg_anat2mni.py, can use this to transform any image
+in functional space to template space (MNI or custom).
+
+input
+    Path to an image in the space of the reference functional scan.
+    May be a single volume or a timeseries.
+
+output
+    Filename for image to be written in template space.
+
+subject
+    Subject code. Used to look up existing transforms. Must also
+    set the STUDYDIR environment variable as the base directory
+    for the study before calling.
+
+OPTIONS
+-a
+    Suffix of anatomical image series that was used as reference
+    When registration was done to the template. Should also be the
+    image used as a target for registering the reference functional
+    scan to the anatomical. If not specified, no suffix will be
+    used.
+
+-m
+    Input image is a mask. If set, will use nearest neighbor
+    interpolation. Otherwise, B-spline interpolation will be used.
+
+-p
+    Post-mask to apply to the transformed image. This is useful
+    to remove very small values outside the brain that occur when
+    using B-spline interpolation.
+
+EOF
     exit 1
 fi
 
