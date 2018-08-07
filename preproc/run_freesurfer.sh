a3ff719e4058c4e3ebd3f46581bdba3b5ab949ee
Neal Morton <mortonne@gmail.com>
2018-08-07 16:12:52 -0500

    support for hippocampal subfield estimation

--- preproc/run_freesurfer.sh
+++ preproc/run_freesurfer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "Usage: run_freesurfer.sh [-f] [-o outname] subject nthreads"
+    echo "Usage: run_freesurfer.sh [-cf] [-o outname] subject nthreads"
     echo
     echo "Use highres anatomical image to run FreeSurfer standard"
     echo "reconstruction. Highres is expected to be in:"
@@ -11,6 +11,11 @@ if [ $# -lt 2 ]; then
     echo "hemisphere during processing."
     echo
     echo 'Results are saved in: $STUDYDIR/$subject/anatomy/$outname'
+    echo
+    echo "-c"
+    echo "    Use a T2 coronal image to estimate hippocampal subfields."
+    echo "    Coronal image expected to be in:"
+    echo '    $STUDYDIR/$subject/anatomy/coronal.nii.gz'
     echo
     echo "-f"
     echo "    Use a FLAIR image to refine the pial surface. Expected to"
@@ -32,11 +37,15 @@ if [ ! -d $STUDYDIR ]; then
     exit 1
 fi
 
+coronal=false
 flair=false
-while getopts ":o:f" opt; do
+while getopts ":o:cf" opt; do
     case $opt in
 	o)
 	    outname=$OPTARG
+	    ;;
+	c)
+	    coronal=true
 	    ;;
 	f)
 	    flair=true
@@ -65,17 +74,21 @@ if [ -d ${subjdir}/anatomy/$outname ]; then
 fi
 
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+opt=""
 if [ $flair = true ]; then
     if [ ! -e $subjdir/anatomy/flair.nii.gz ]; then
 	echo "Error: flair image not found."
 	exit 1
     fi
-    recon-all -s ${outname} -sd ${subjdir}/anatomy/ \
-	      -i ${subjdir}/anatomy/highres.nii.gz -all \
-	      -FLAIR $subjdir/anatomy/flair.nii.gz -FLAIRpial \
-	      -parallel -openmp $nthreads -itkthreads $nthreads
-else
-    recon-all -s ${outname} -sd ${subjdir}/anatomy/ \
-	      -i ${subjdir}/anatomy/highres.nii.gz -all \
-	      -parallel -openmp $nthreads -itkthreads $nthreads
+    opt="$opt -FLAIR $subjdir/anatomy/flair.nii.gz -FLAIRpial"
 fi
+if [ $coronal = true ]; then
+    if [ ! -e $subjdir/anatomy/coronal.nii.gz ]; then
+	echo "Error: coronal image not found."
+	exit 1
+    fi
+    opt="$opt -hippocampal-subfields-T2 $subjdir/anatomy/coronal.nii.gz T2"
+fi
+
+recon-all -s ${outname} -sd ${subjdir}/anatomy/ -i ${subjdir}/anatomy/highres.nii.gz -all -parallel -openmp $nthreads -itkthreads $nthreads $opt
