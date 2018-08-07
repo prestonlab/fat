a9796c4fd6c51cac56158d16b2a5f6bde2d9be91
Neal Morton <mortonne@gmail.com>
2018-08-07 16:09:51 -0500

    change to be similar to the new version of transform_level1

--- model/identity_level1.py
+++ model/identity_level1.py
@@ -5,34 +5,31 @@ import os
 from subjutil import *
 
 parser = SubjParser()
-parser.add_argument('model', help="name of model")
-parser.add_argument('--feat-pattern', '-f',
-                    help="regular expression for feat directories (default: ^\D+_\d+\.feat$)",
-                    metavar='regexp',
-                    default='.*\.feat$')
-parser.add_argument('--anat', '-a',
-                    default="anatomy/bbreg/data/orig_brain",
-                    help="path to anatomical image in functional space, relative to subject directory (default: anatomy/bbreg/data/orig_brain)")
+parser.add_argument('featdir', help="path to feat directory")
 args = parser.parse_args()
 
+featname = os.path.splitext(os.path.basename(args.featdir))[0]
+
 sp = SubjPath(args.subject, args.study_dir)
-log = sp.init_log('%s_level1_ident' % args.model, 'model', args)
+log = SubjLog(args.subject, 'identlev1_' + featname, 'model',
+              args.clean_logs, args.dry_run)
 
-feat_dirs = sp.feat_dirs(args.model, args.feat_pattern)
+# highres image for rendering level 2 results
+highres = sp.image_path('anatomy', 'bbreg', 'data', 'orig_brain')
 
-highres = impath(sp.subj_dir, args.anat)
+# identity transform that will leave images in native space
 identity = sp.proj_path('resources', 'identity.mat')
 
 log.start()
-for feat in feat_dirs:
-    reg_dir = os.path.join(feat, 'reg')
-    log.run('mkdir -p %s' % reg_dir)
-    log.run('ln -sf %s %s' % (highres, impath(reg_dir, 'highres')))
-    log.run('ln -sf %s %s' % (highres, impath(reg_dir, 'standard')))
-    log.run('cp %s %s' % (
-        identity, os.path.join(reg_dir, 'standard2example_func.mat')))
-    log.run('cp %s %s' % (
-        identity, os.path.join(reg_dir, 'example_func2standard.mat')))
-    log.run('updatefeatreg %s -pngs' % feat)
+
+reg_dir = os.path.join(args.featdir, 'reg')
+log.run('mkdir -p {}'.format(reg_dir))
+log.run('ln -sf {} {}'.format(highres, impath(reg_dir, 'highres')))
+log.run('ln -sf {} {}'.format(highres, impath(reg_dir, 'standard')))
+log.run('cp {} {}'.format(
+    identity, os.path.join(reg_dir, 'standard2example_func.mat')))
+log.run('cp {} {}'.format(
+    identity, os.path.join(reg_dir, 'example_func2standard.mat')))
+log.run('updatefeatreg {} -pngs'.format(args.featdir))
 
 log.finish()
