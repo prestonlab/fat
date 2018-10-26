9aa84ceb4e38f4ed2536b23ee58d0348baecc8a3
Neal Morton <mortonne@gmail.com>
2018-10-26 18:38:14 -0500

    fix for if there is just one anatomical scan

--- preproc/reg_freesurfer.py
+++ preproc/reg_freesurfer.py
@@ -12,28 +12,33 @@ log = sp.init_log('postfs', 'preproc', args)
 
 log.start()
 
-# look for registration between scans
-dest = sp.path('anatomy')
-reg_dir_list = glob(os.path.join(dest, 'highres?_highres?'))
-if not reg_dir_list:
-    raise IOError('Cannot find registration between different highres scans.')
-elif len(reg_dir_list) > 1:
-    raise IOError('Found multiple registrations between scans.')
-reg_dir = reg_dir_list[0]
-
-# unpack which scan was used as reference in the registration
-movname, fixname = os.path.basename(reg_dir).split('_')
-movnum = movname.split('highres')[1]
-fixnum = fixname.split('highres')[1]
-
 # register orig to highres
+dest = sp.path('anatomy')
 log.run('antsRegistration -d 3 -r [{ref},{src},1] -t Rigid[0.1] -m MI[{ref},{src},1,32,Regular,0.25] -c [1000x500x250x100,1e-6,10] -f 8x4x2x1 -s 3x2x1x0vox -n BSpline -w [0.005,0.995] -o {xfm}'.format(
         ref=impath(dest, 'highres'), src=impath(dest, 'orig'),
         xfm=os.path.join(dest, 'orig-highres_')))
 o2h = os.path.join(dest, 'orig-highres_0GenericAffine.mat')
 
-h2h_affine = os.path.join(dest, reg_dir, 'mov-fix_0GenericAffine.mat')
-h2h_warp = os.path.join(dest, reg_dir, 'mov-fix_1InverseWarp.nii.gz')
+# look for registration between scans
+anat_list = glob(os.path.join(dest, 'highres?.nii.gz'))
+reg_dir_list = glob(os.path.join(dest, 'highres?_highres?'))
+if not anat_list:
+    coreg = False
+else:
+    if len(reg_dir_list) > 1:
+        raise IOError('Found multiple registrations between scans.')
+
+    coreg = True
+    reg_dir = reg_dir_list[0]
+    
+    # unpack which scan was used as reference in the registration
+    movname, fixname = os.path.basename(reg_dir).split('_')
+    movnum = movname.split('highres')[1]
+    fixnum = fixname.split('highres')[1]
+
+    h2h_affine = os.path.join(dest, reg_dir, 'mov-fix_0GenericAffine.mat')
+    h2h_warp = os.path.join(dest, reg_dir, 'mov-fix_1InverseWarp.nii.gz')
+
 images = ['orig', 'orig_brain', 'brainmask', 'aparc+aseg',
           'aparc.a2009s+aseg', 'aparc.DKTatlas+aseg', 'ctx', 'wm']
 labels = [False, False, True, True,
@@ -43,15 +48,28 @@ for image, label in zip(images, labels):
         interp = 'NearestNeighbor'
     else:
         interp = 'BSpline'
-        
-    # transform to the space of the moving image
-    log.run('antsApplyTransforms -i {} -o {} -r {} -t [{},1] -t {} -t {} -n {}'.format(
-        impath(dest, image), impath(dest, image + movnum),
-        impath(dest, 'highres' + movnum), h2h_affine, h2h_warp, o2h, interp))
 
-    # transform to the space of the fixed image
-    log.run('antsApplyTransforms -i {} -o {} -r {} -t {} -n {}'.format(
-        impath(dest, image), impath(dest, image + fixnum),
-        impath(dest, 'highres' + fixnum), o2h, interp))
+    src_image = impath(dest, image)
+    if not os.path.exists(src_image):
+        print("Image does not exist: {}".format(src_image))
+        continue
+        
+    if not coreg:
+        # just transform to the space of the original highres
+        # scan. Will add a "1" to all images to distinguish from the
+        # freesurfer-space image
+        log.run('antsApplyTransforms -i {} -o {} -r {} -t {} -n {}'.format(
+            impath(dest, image), impath(dest, image + '1'),
+            impath(dest, 'highres'), o2h, interp))
+    else:
+        # transform to the space of the moving image
+        log.run('antsApplyTransforms -i {} -o {} -r {} -t [{},1] -t {} -t {} -n {}'.format(
+            impath(dest, image), impath(dest, image + movnum),
+            impath(dest, 'highres' + movnum), h2h_affine, h2h_warp, o2h, interp))
+
+        # transform to the space of the fixed image
+        log.run('antsApplyTransforms -i {} -o {} -r {} -t {} -n {}'.format(
+            impath(dest, image), impath(dest, image + fixnum),
+            impath(dest, 'highres' + fixnum), o2h, interp))
 
 log.finish()
