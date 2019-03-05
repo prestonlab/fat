9a5d40c908226798fb21b3c78c5d24b5b9671316
tonydutcher <amdutcher@utexas.edu>
2019-03-05 12:03:43 -0600

    script to calculate voxelwise tsnr for the wholebrain

---
+++ preproc/tsnr_wb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+echo "Usage: tsnr_wb.sh <input image> <subject>"
+echo "Example: tsnr_wb.sh /absolute/path/rest_23_denoise No_001"
+echo "Description: Calculates the temporal snr for whole brain "
+echo "             and saves in corresponding bold dir."
+exit 1
+fi
+
+input=$1
+subject=$2
+
+# input and output directories
+parentdir=$(dirname $input)
+image=$(basename $input .nii.gz)
+
+cd $parentdir
+fslmaths $image -Tmean -mas mask tmp_avg
+fslmaths $image -Tstd -mas mask tmp_std
+fslmaths tmp_avg -div tmp_std ${image}_tsnr_wb
+rm tmp_*
