aabca9b857e774b786b0d8acc4bb5f441991f735
Neal Morton <mortonne@gmail.com>
2019-12-16 15:40:51 -0600

    support for displaying job progress; disable command dump by default

--- utils/running_jobs.sh
+++ utils/running_jobs.sh
@@ -2,11 +2,15 @@
 #
 # Print status about running jobs submitted through launch.
 
-while getopts ":n:" opt; do
+show_cmds=false
+while getopts ":sn:" opt; do
     case $opt in
-	n)
-	    lines=$OPTARG
-	    ;;
+        s)
+            show_cmds=true
+            ;;
+        n)
+            lines=$OPTARG
+            ;;
         *)
             echo "Invalid option: $opt"
             exit 1
@@ -15,29 +19,46 @@ while getopts ":n:" opt; do
 done
 shift $((OPTIND-1))
 
-job_dir=${BATCHDIR}
+job_dir="${BATCHDIR}"
+
+# get a list of names for all running or pending jobs
 running=$(squeue -u $USER -h -o "%j" -S i -t RUNNING,PENDING)
 for f in $running; do
+    # look up queue information about this job
     status=$(squeue -u $USER -n $f -h -o %T)
     elapsed=$(squeue -u $USER -n $f -h -o %M)
     limit=$(squeue -u $USER -n $f -h -o %l)
-    file=${job_dir}/${f}
-    if [ -e ${file}.sh ]; then
-	ext=.sh
+
+    # check for a command file with the same name
+    file="${job_dir}/${f}"
+    if [[ -e ${file}.sh ]]; then
+        ext=.sh
     else
-	ext=.txt
+        ext=.txt
     fi
-	
-    if [ -e ${file}${ext} ]; then
-	echo "$f ($status $elapsed/$limit):"
-	if [ -n "$lines" ]; then
-	    head -n $lines ${file}${ext}
-	else
-	    while read line; do
-		echo $line
-	    done < ${file}${ext}
-	fi
+
+    # if there is an output file, check how many commands are finished
+    if [[ -e ${file}.out ]]; then
+        # assume standard naming as in ezlaunch, slaunch, rlaunch
+        n_command=$(wc -l "${file}.sh" | cut -d ' ' -f 1)
+        n_finished=$(grep -c 'Launcher: Job [[:digit:]]* completed in [[:digit:]]* seconds.' "${file}.out")
+    fi
+
+    # progress summary
+    if [[ $n_finished ]]; then
+        echo "$f ($status $n_finished/$n_command $elapsed/$limit)"
     else
-	echo "$f ($status $elapsed/$limit)"
+        echo "$f ($status $elapsed/$limit)"
+    fi
+
+    # list all commands
+    if [[ -e ${file}${ext} && $show_cmds = true ]]; then
+        if [[ $lines ]]; then
+            head -n $lines ${file}${ext}
+        else
+            while read line; do
+                echo $line
+            done < "${file}${ext}"
+        fi
     fi
 done
