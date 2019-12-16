842f5eca504e9b5395c32dc2486566d2e2e262bc
Neal Morton <mortonne@gmail.com>
2019-12-16 15:14:56 -0600

    style fixes

--- utils/running_jobs.sh
+++ utils/running_jobs.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+#
+# Print status about running jobs submitted through launch.
 
 while getopts ":n:" opt; do
     case $opt in
 	n)
 	    lines=$OPTARG
 	    ;;
+        *)
+            echo "Invalid option: $opt"
+            exit 1
+            ;;
     esac
 done
 shift $((OPTIND-1))
 
 job_dir=${BATCHDIR}
-running=`squeue -u $USER -h -o "%j" -S i -t RUNNING,PENDING`
+running=$(squeue -u $USER -h -o "%j" -S i -t RUNNING,PENDING)
 for f in $running; do
-    status=`squeue -u $USER -n $f -h -o %T`
-    elapsed=`squeue -u $USER -n $f -h -o %M`
-    limit=`squeue -u $USER -n $f -h -o %l`
+    status=$(squeue -u $USER -n $f -h -o %T)
+    elapsed=$(squeue -u $USER -n $f -h -o %M)
+    limit=$(squeue -u $USER -n $f -h -o %l)
     file=${job_dir}/${f}
     if [ -e ${file}.sh ]; then
 	ext=.sh
